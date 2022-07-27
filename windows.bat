9e5b38e71b15360d27b1fda8814d3c490c1b6aaf
Johnny O'Donnell <odjohnny@amazon.com>
2022-07-27 13:18:15 -0700

    Add windows uploader

---
+++ windows.bat
@@ -0,0 +1,14 @@
+
+@echo off
+echo What is the title of this key?
+set /p title=
+echo.
+
+echo Paste your Access token here:
+set /p token=
+echo.
+
+set /p ssh_key=<C:\Users\%USERNAME%\.ssh\id_rsa.pub
+
+curl -H "Authorization: token %token%" --data "{\"title\": \"%title%\", \"key\": \"%ssh_key%\"}" https://api.github.com/user/keys
+
