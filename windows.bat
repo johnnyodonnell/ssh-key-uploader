df9517e5819ec60d35d6aa402025d9bc644303b4
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2023-08-08 13:59:09 -0700

    Escape backslashes

--- windows.bat
+++ windows.bat
@@ -10,5 +10,9 @@ echo.
 
 set /p ssh_key=<C:\Users\%USERNAME%\.ssh\id_rsa.pub
 
+:: Escape backslash to account for user names with backslashes, such as
+:: domain\username@computername
+set ssh_key=%ssh_key:\=\\%
+
 curl -H "Authorization: token %token%" --data "{\"title\": \"%title%\", \"key\": \"%ssh_key%\"}" https://api.github.com/user/keys
 
